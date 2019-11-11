6757ed45177db50f04c86e06cf6eed9f19c348b7
AhmedEid3 <ahmed.eid3@outlook.com>
2019-11-11 19:31:18 +0200

    add new files

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
+    $('.modal').modal();
+
+    el = document.querySelector('.tabs');
+    let instance = M.Tabs.init(el);
+
+
+    $('.modal-trigger').click(function () {
+        instance.select('test2');
+        instance.updateTabIndicator();
+    });
+
 });
+
+
