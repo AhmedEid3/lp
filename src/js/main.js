6fa5e15c778980525f8d7520947fdf4320bafa12
AhmedEid3 <ahmed.eid3@outlook.com>
2019-11-12 11:17:28 +0200

    minifiy code

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,6 @@
 $(document).ready(function () {
-    // $('.sidenav').sidenav();
     $('.modal').modal();
     $('.tabs').tabs();
-
-    // el = document.querySelector('.tabs');
-    // let instance = M.Tabs.init(el);
-    //
-    //
-    // $('.modal-trigger').click(function () {
-    //     instance.select('test2');
-    //     instance.updateTabIndicator();
-    // });
-
 });
 
 
